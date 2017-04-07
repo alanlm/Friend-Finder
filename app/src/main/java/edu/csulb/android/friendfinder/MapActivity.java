6e33886b1f5a56c1277b46f29e489a5b212ba571
Alan Manalang <a_rabbit93@yahoo.com>
2017-04-07 11:14:59 -0700

    initialize map settings

--- app/src/main/java/edu/csulb/android/friendfinder/MapActivity.java
+++ app/src/main/java/edu/csulb/android/friendfinder/MapActivity.java
@@ -1,20 +1,28 @@
 package edu.csulb.android.friendfinder;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
 
 public class MapActivity extends Activity implements OnMapReadyCallback {
 
-    MapFragment mapFrag;
+    private MapFragment mapFrag;
+    private GoogleMap gMap;
+    private UiSettings mapSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        // reference to map fragment
         mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this); // sets the callback on the fragment
     }
@@ -26,5 +34,13 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
         *           .position(new LatLng(0, 0))
         *           .title("Marker"));
         */
+
+        gMap = googleMap;
+
+        // initializing Google Map UI settings
+        mapSettings = gMap.getUiSettings();
+        mapSettings.setZoomControlsEnabled(true);
+        mapSettings.setCompassEnabled(true);
     }
+
 }
