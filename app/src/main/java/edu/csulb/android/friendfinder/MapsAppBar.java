5a3994f0cb303804910c8d507a91a63c143145c4
Alan Manalang <a_rabbit93@yahoo.com>
2017-04-11 03:14:26 -0700

    refactored code and added Log statements

--- app/src/main/java/edu/csulb/android/friendfinder/MapsAppBar.java
+++ app/src/main/java/edu/csulb/android/friendfinder/MapsAppBar.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MapsAppBar extends AppCompatActivity {
 
@@ -16,8 +19,20 @@ public class MapsAppBar extends AppCompatActivity {
         ActionBar mapsBar = getSupportActionBar();
         mapsBar.setDisplayShowHomeEnabled(false);
         mapsBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        mapsBar.setCustomView(getLayoutInflater().inflate(R.layout.custom_app_bar, null));
+        // reference to custom bar layout
+        View customBarView = getLayoutInflater().inflate(R.layout.custom_app_bar, null);
+        mapsBar.setCustomView(customBarView);
         mapsBar.setDisplayShowCustomEnabled(true);
+
+        // reference to ImageButton
+        ImageButton friendsListIcon = (ImageButton) customBarView.findViewById(R.id.bar_friends_button);
+        // handle friends list click event
+        friendsListIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("FRIENDS LIST ICON", "You clicked on Friends List Image Button");
+            }
+        });
     }
 
     @Override
